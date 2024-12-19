da7d4605b8fd4eda0f5e3d4e4ee493f1b419c663
melika khosravi <m.khsrv1998@gmail.com>
2024-12-19 06:10:10 -0500

    fifth commit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask , redirect, url_for , render_template , request
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
 
 @app.route("/")
 def home():
